f65dfae4c6b4cca23e933ccd765424b9c9e7aa9f
JIMHWANG <jimhwang1129@gmail.com>
2022-09-11 00:45:56 +0800

    待辦事項顯示+layout

--- src/Components/Function.js
+++ src/Components/Function.js
@@ -1,8 +1,46 @@
 import Home_Login from "./Home_Login";
 import "../Style/Function.scss"
 import { Link } from "react-router-dom";
+import React from "react";
+
+const { useState, useEffect } = React;
+
+
+function ShowItems({ ToDoListItems, ItemDisplayValue }) {
+    return (
+        <ul className={`${!ItemDisplayValue}ItemDisPlay`}>
+            {
+                ToDoListItems.map((ToDoListItem, Index) => {
+
+                    return (
+                        <li key={Index}>
+                            <input type="checkbox" name="" id="" />
+                            {ToDoListItem.item}
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
+}
+
+
 
 function MainFunction() {
+
+    const [ToDoListItems, setToDoListItems] = useState([]);
+    const [InputItem, setInputItem] = useState('');
+    const [ItemDisplayValue, setItemDisplayValue] = useState(false);
+
+    useEffect(() => {
+
+        if (Object.keys(ToDoListItems).length === 0) {
+            setItemDisplayValue(true);
+        } else {
+            setItemDisplayValue(false);
+        }
+    }, [ToDoListItems])
+
     return (
         <>
             <h1>
@@ -10,14 +48,26 @@ function MainFunction() {
             </h1>
             <div className="ToDoList_Container">
                 <div className="ToDoList_Input">
-                    <input type="text" placeholder="請輸入代辦事項" />
-                    <a href="#"><i></i></a>
+                    <input type="text" placeholder="請輸入代辦事項" value={InputItem} onChange={(e) => {
+                        setInputItem(e.target.value);
+                    }} />
+                    <a href="#" onClick={() => {
+                        if (!InputItem) { return; }
+
+                        setToDoListItems([...ToDoListItems, { item: InputItem, completed: false }]);
+                        setInputItem('');
+
+                    }}><i></i></a>
                 </div>
                 <div className="ToDoList_Content">
                     <div className="ToDoList_ItemStatus">
-                        <Link to="/">全部</Link>
-                        <Link to="/">待完成</Link>
-                        <Link to="/">已完成</Link>
+                        <Link to="#">全部</Link>
+                        <Link to="#">待完成</Link>
+                        <Link to="#">已完成</Link>
+                    </div>
+                    <div className="ToDoList_Items">
+                        <p className={`NoItems ${ItemDisplayValue}ItemDisPlay`}>目前尚無代辦事項</p>
+                        <ShowItems ToDoListItems={ToDoListItems} ItemDisplayValue={ItemDisplayValue} />
                     </div>
                 </div>
             </div>
