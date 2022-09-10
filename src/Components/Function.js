71a1b2fa96ca020450982787fd42698c9e7192c5
JIMHWANG <jimhwang1129@gmail.com>
2022-09-10 13:07:22 +0800

    Function頁 Input + Item狀態Layout

--- src/Components/Function.js
+++ src/Components/Function.js
@@ -1,6 +1,27 @@
+import Home_Login from "./Home_Login";
+import "../Style/Function.scss"
+import { Link } from "react-router-dom";
+
 function MainFunction() {
     return (
-        <p>This is Function Page</p>
+        <>
+            <h1>
+                <Link to="/">Online ToDoList</Link>
+            </h1>
+            <div className="ToDoList_Container">
+                <div className="ToDoList_Input">
+                    <input type="text" placeholder="請輸入代辦事項" />
+                    <a href="#"><i></i></a>
+                </div>
+                <div className="ToDoList_Content">
+                    <div className="ToDoList_ItemStatus">
+                        <Link to="/">全部</Link>
+                        <Link to="/">待完成</Link>
+                        <Link to="/">已完成</Link>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
